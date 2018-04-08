1481527e1db3e9af32ac8d5c773b30f16201bb13
falafeless <nwelchr@gmail.com>
2018-04-08 13:41:07 -0700

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ nick = User.create!(
     profile_picture: 'https://scontent.xx.fbcdn.net/v/t1.0-1/c17.0.64.64/p64x64/21751274_10155715933012733_239784575756338336_n.jpg?oh=2aeb2d164b3bd8ff56afe1331657053f&oe=5B38CB97'
 );
 
-GENRES = ['pop', 'rock', 'smooth']
+GENRES = ['pop', 'rock', 'smooth', 'electronic']
 
 azealia = Artist.create!(
     name: 'Azealia Banks',
@@ -432,6 +432,423 @@ frank_playlist_songs.each do |frank_song|
         song_id: frank_song.id
     )
 end
+
+sophie = Artist.create!(
+    name: 'SOPHIE',
+    artist_artwork_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Artist+Artwork/sophie.jpg',
+    background_artwork_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Artist+Artwork/sophie_background.jpeg',
+    genre_id: 4,
+    about: 'SOPHIE\'s about'
+)
+
+msmsmsm = Album.create!(
+    title: 'MSMSMSM',
+    year: 2015,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/MSMSMSM/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'MSMSMSM',
+    album_id: msmsmsm.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/MSMSMSM/MSMSMSM.mp3'
+)
+
+just_like_we_never = Album.create!(
+    title: 'JUST LIKE WE NEVER SAID GOODBYE',
+    year: 2015,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/JUST+LIKE+WE+NEVER+SAID+GOODBYE/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'JUST LIKE WE NEVER SAID GOODBYE',
+    album_id: just_like_we_never.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/JUST+LIKE+WE+NEVER+SAID+GOODBYE/JUST+LIKE+WE+NEVER+SAID+GOODBYE.mp3'
+)
+
+love = Album.create!(
+    title: 'L.O.V.E.',
+    year: 2015,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/L.O.V.E./album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'L.O.V.E.',
+    album_id: love.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/L.O.V.E./L.O.V.E..mp3'
+)
+
+vyzee = Album.create!(
+    title: 'VYZEE',
+    year: 2015,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/VYZEE/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'VYZEE',
+    album_id: vyzee.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/VYZEE/SOPHIE+-+VYZEE+(Official+Stream).mp3'
+)
+
+lemonade_s = Album.create!(
+    title: 'LEMONADE',
+    year: 2015,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/LEMONADE/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'LEMONADE',
+    album_id: lemonade_s.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/LEMONADE/LEMONADE.mp3'
+)
+
+hard = Album.create!(
+    title: 'HARD',
+    year: 2015,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/HARD/album_cover.png' 
+)
+
+Song.create!(
+    title: 'HARD',
+    album_id: hard.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/HARD/HARD.mp3'
+)
+
+
+elle = Album.create!(
+    title: 'ELLE',
+    year: 2015,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/ELLE/album_cover.png' 
+)
+
+Song.create!(
+    title: 'ELLE',
+    album_id: elle.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/ELLE/ELLE.mp3'
+)
+
+bipp = Album.create!(
+    title: 'BIPP',
+    year: 2015,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/BIPP/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'BIPP',
+    album_id: bipp.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/BIPP/BIPP.mp3'
+)
+
+its_ok_to_cry = Album.create!(
+    title: 'It\'s Okay To Cry',
+    year: 2017,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/It\'s+Okay+To+Cry/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'It\'s Okay To Cry',
+    album_id: its_ok_to_cry.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/It\'s+Okay+To+Cry/it\'s+okay+to+cry.mp3'
+)
+
+ponyboy = Album.create!(
+    title: 'Ponyboy',
+    year: 2017,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Ponyboy/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'Ponyboy',
+    album_id: ponyboy.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Ponyboy/ponyboy.mp3'
+)
+
+faceshopping = Album.create!(
+    title: 'Faceshopping',
+    year: 2017,
+    artist_id: sophie.id,
+    album_type: 'Single',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Faceshopping/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'Faceshopping',
+    album_id: faceshopping.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Faceshopping/faceshopping.mp3'
+)
+
+sophie_playlist = Playlist.create!(
+    name: 'SOPHIE - PRODUCT',
+    creator_id: demo.id
+)
+
+sophie_songs = sophie.songs
+
+sophie_songs.each do |sophie_song|
+    SavedSong.create!(
+        playlist_id: sophie_playlist.id,
+        song_id: sophie_song.id
+    )
+end
+
+grimes = Artist.create!(
+    name: 'Grimes',
+    artist_artwork_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Artist+Artwork/grimes.png',
+    background_artwork_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Artist+Artwork/grimes_background.jpg',
+    genre_id: 4,
+    about: 'SOPHIE\'s about'
+)
+
+art_angels = Album.create!(
+    title: 'Art Angels',
+    year: 2015,
+    artist_id: grimes.id,
+    album_type: 'Album',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'laughing and not being normal',
+    album_id: art_angels.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/01.+laughing+and+not+being+normal.mp3'
+)
+
+Song.create!(
+    title: 'California',
+    album_id: art_angels.id,
+    ord: 2,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/02.+California.mp3'
+)
+
+Song.create!(
+    title: 'SCREAM',
+    album_id: art_angels.id,
+    ord: 3,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/03.+SCREAM.mp3'
+)
+
+Song.create!(
+    title: 'Flesh without Blood',
+    album_id: art_angels.id,
+    ord: 4,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/04.+Flesh+without+Blood.mp3'
+)
+
+Song.create!(
+    title: 'Belly of the Beat',
+    album_id: art_angels.id,
+    ord: 5,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/05.+Belly+of+the+Beat.mp3'
+)
+
+Song.create!(
+    title: 'Kill v. Maim',
+    album_id: art_angels.id,
+    ord: 6,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/06.+Kill+V.+Maim.mp3'
+)
+
+Song.create!(
+    title: 'Artangels',
+    album_id: art_angels.id,
+    ord: 7,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/07.+Artangels.mp3'
+)
+
+Song.create!(
+    title: 'Easily',
+    album_id: art_angels.id,
+    ord: 8,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/08.+Easily.mp3'
+)
+
+Song.create!(
+    title: 'Pin',
+    album_id: art_angels.id,
+    ord: 9,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/09.+Pin.mp3'
+)
+
+Song.create!(
+    title: 'Realiti',
+    album_id: art_angels.id,
+    ord: 10,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/10.+Realiti.mp3'
+)
+
+Song.create!(
+    title: 'World Princess part II',
+    album_id: art_angels.id,
+    ord: 11,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/11.+World+Princess+part+II.mp3'
+)
+
+Song.create!(
+    title: 'Venus Fly',
+    album_id: art_angels.id,
+    ord: 12,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/12.+Venus+Fly.mp3'
+)
+
+Song.create!(
+    title: 'Life in the Vivid Dream',
+    album_id: art_angels.id,
+    ord: 13,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/13.+Life+in+the+Vivid+Dream.mp3'
+)
+
+Song.create!(
+    title: 'Butterfly',
+    album_id: art_angels.id,
+    ord: 14,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Grimes+-+Art+Angels+(2015)/14.+Butterfly.mp3'
+)
+
+grimes_playlist = Playlist.create!(
+    name: 'Grimes Playlist',
+    creator_id: nick.id
+)
+
+grimes_songs = grimes.songs
+
+grimes_songs.each do |grimes_song|
+    SavedSong.create!(
+        playlist_id: grimes_playlist.id,
+        song_id: grimes_song.id
+    )
+end
+
+tennis = Artist.create!(
+    name: 'Tennis',
+    artist_artwork_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Artist+Artwork/tennis.jpeg',
+    background_artwork_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Artist+Artwork/tennis_background.png',
+    genre_id: 4,
+    about: 'Tennis\' about'
+)
+
+yours_conditionally = Album.create!(
+    title: 'Yours Conditionally',
+    year: 2017,
+    artist_id: tennis.id,
+    album_type: 'Album',
+    album_cover_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/album_cover.jpg' 
+)
+
+Song.create!(
+    title: 'In the Morning I\'ll Be Better',
+    album_id: yours_conditionally.id,
+    ord: 1,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/01+In+The+Morning+I\'ll+Be+Better.mp3'
+)
+
+Song.create!(
+    title: 'My Emotions are Blinding',
+    album_id: yours_conditionally.id,
+    ord: 2,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/02+My+Emotions+Are+Blinding.mp3'
+)
+
+Song.create!(
+    title: 'Fields of Blue',
+    album_id: yours_conditionally.id,
+    ord: 3,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/03+Fields+of+Blue.mp3'
+)
+
+Song.create!(
+    title: 'Ladies Don\'t Play Guitar',
+    album_id: yours_conditionally.id,
+    ord: 4,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/04+Ladies+Don\'t+Play+Guitar.mp3'
+)
+
+Song.create!(
+    title: 'Matrimony',
+    album_id: yours_conditionally.id,
+    ord: 5,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/05+Matrimony.mp3'
+)
+
+Song.create!(
+    title: 'Baby Don\'t Believe',
+    album_id: yours_conditionally.id,
+    ord: 6,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/06+Baby+Don\'t+Believe.mp3'
+)
+
+Song.create!(
+    title: 'Please Don\'t Ruin This for Me',
+    album_id: yours_conditionally.id,
+    ord: 7,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/07+Please+Don\'t+Ruin+This+For+Me.mp3'
+)
+
+Song.create!(
+    title: '10 Minutes 10 Years',
+    album_id: yours_conditionally.id,
+    ord: 8,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/08+10+Minutes+10+Years.mp3'
+)
+
+Song.create!(
+    title: 'Modern Woman',
+    album_id: yours_conditionally.id,
+    ord: 9,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/09+Modern+Woman.mp3'
+)
+
+Song.create!(
+    title: 'Island Music',
+    album_id: yours_conditionally.id,
+    ord: 10,
+    mp3_url: 'https://s3.us-east-2.amazonaws.com/bop-songs/Tennis+-+Yours+Conditionally+(2017)/10+Island+Music.mp3'
+)
+
+tennis_playlist = Playlist.create!(
+    name: 'Tennis Playlist',
+    creator_id: demo.id
+)
+
+tennis_songs = tennis.songs
+
+tennis_songs.each do |tennis_song|
+    SavedSong.create!(
+        playlist_id: tennis_playlist.id,
+        song_id: tennis_song.id
+    )
+end
+
 
 normal_playlist = Playlist.create!(
     name: 'Demo Playlist',
