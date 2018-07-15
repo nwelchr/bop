6712db31d859d14bd82664cd11f0301ebdb3ba8e
nwelchr <nwelchr@gmail.com>
2018-07-15 14:32:37 -0700

    Clean up entry file

--- frontend/bop.jsx
+++ frontend/bop.jsx
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { fetchPlaylists } from './actions/playlist_actions';
-
-import { saveSongToPlaylist } from './actions/song_actions';
-
-import { fetchSearchResults } from './actions/search_actions';
-
-import { fetchSong } from './actions/song_actions';
-import { playSong } from './actions/audio_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
@@ -21,17 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-
-  // for testing
-  window.fetchSearchResults = fetchSearchResults;
-  window.saveSongToPlaylist = saveSongToPlaylist;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.fetchSong = fetchSong;
-  window.playSong = playSong;
-  //
-
-  window.fetchPlaylists = fetchPlaylists;
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
